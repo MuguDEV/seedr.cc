a11854b01a361dd0b075278c6c328b3f09e8e644
MuguDEV <the.mugunthan@gmail.com>
2024-04-12 18:04:24 +0530

    modified:   seedr/errors.py

--- seedr/errors.py
+++ seedr/errors.py
@@ -2,24 +2,6 @@
 Module for custom Errors
 """
 
-import requests
-
-
-def reset_password(email: str, timeout=10) -> None:
-    """
-    Sends a password reset request to the Seedr API.
-
-    Args:
-        email (str): The email address associated with the account.
-        timeout (int, optional): The timeout for the HTTP request in seconds. Defaults to 10.
-    """
-    response = requests.post(
-        "https://www.seedr.cc/auth/password-recover/request",
-        data={"email": email},
-        timeout=timeout,
-    )
-    if response.ok:
-        print("Password recovery email has been sent")
 
 
 class InvalidEmailError(Exception):
